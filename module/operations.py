f8e6e5bfd7c2a082634e28ec7891ac863ec4b574
Devante Rose <drose2021@yahoo.com>
2023-08-11 02:30:27 -0400

    first commit

---
+++ module/operations.py
@@ -0,0 +1,14 @@
+def house_area():
+    length = int(input("enter length of the house: "))
+    width = int(input("enter width of the house: "))
+    area = length * width
+    print(area)
+house_area()
+
+
+def c_circle():
+    pi = 3.14159
+    radius = int(input("enter radius of the circle: "))
+    circumfrence = 2 * pi * radius
+    print(circumfrence)   
+house_area()
